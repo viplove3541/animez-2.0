a16b8bb4cb95176417eeada0267dfbbed132bba1
Viplove Kale <80448124+viplove3541@users.noreply.github.com>
2025-03-29 12:15:11 +0530

    Update useConsumet.jsx

--- src/hooks/useConsumet.jsx
+++ src/hooks/useConsumet.jsx
@@ -4,7 +4,7 @@ import { servers } from "../api/gogoanime_servers";
 
 // Define a custom hook for handling API consumption and responses
 export const useHandleConsumetResponse = (endpoint, parameter) => {
-  const BASE_URL = "https://api.consumet.manjotbenipal.xyz/anime/gogoanime";
+  const BASE_URL = "https://api.consumet.org/anime/gogoanime/watch/spy-x-family-episode-1";
   // 'https://api.consumet.org/anime/gogoanime/servers/'
 
   // Use the 'useQuery' hook to fetch data from the specified API endpoint
